75ee2cc3ff636d454825cd3bfc51563f2e1f39cb
Kunia2016 <hibudy@aol.com>
2016-05-24 22:05:52 -1000

    almost done

--- js/app.js
+++ js/app.js
@@ -1,40 +1,43 @@
 
   /*1.  Fill in the correct id to target the paragraph tag*/
   function fontSize(){
-    document.getElementById().style.fontSize = "200%";
+    document.getElementById("biggie").style.fontSize = "200%";
   }
+
+
 
  /* 2. change the font color to red*/
   function colorMeBadd(){
-    document.getElementById().style.color = something; 
+    document.getElementById("colors").style.color = "red"; 
   }
 
 /*3. fill in the spaces to make this function operate:*/
   function showAnswer(){
-    document.getElementById().something = " C (da sea get it?) arrrr!";
+    document.getElementById("answer").innerHTML = "C (da sea get it?) arrrr!";
   }
 
   /*4.  Create a button that will display the message "My name is Lee, Brrruuuce Lee!" in a new paragraph element after the button is clicked.*/
-function something(){
-  document.getElementById(something).something = "My name is Lee, B<span>rrr</span>uuuce Lee!";
+function kungfu(){
+  document.getElementById("beWater").innerHTML = "My name is Lee, B<span>rrr</span>uuuce Lee!";
 }
  
 //Event Listeners
 //Please write your code inside the window.onload function for exerices 5 onward...
 
-window.onload = function (){
+window.onload = function benjamin(){
+document.getElementById("lines").addEventListener = "lines";
 
-document.getElementById().addEventListener('click', something);
-
-function sayQuote(){
-  something.getElementById().something = "Our lives are defined by opportunities; even the ones we miss.";
+function sayQuote(lines){
+  document.getElementById("Benjamin").innerHTML = "Our lives are defined by opportunities; even the ones we miss.";
 }
+document.getElementById("Benjamin").addEventListener("click", sayQuote);
+//.innerHTML = "Our lives are defined by opportunities; even the ones we miss.";
 
 
 //6.  Generate a random quote from clicking on a button
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance."];
 
-document.getElementById().something();
+document.getElementById('change').addEventListener("click", sayQuotes);
                                                    
 function sayQuotes(){
   var getQuotes = quotes[Math.floor(Math.random()*quotes.length)];
@@ -45,14 +48,20 @@ function sayQuotes(){
 Make a visual list of the items in the orders array. When clicking on the product, you must show and hide the price for that item.*/
 
 
-var orders = [{product: "Space Cat T-shirt", price: 100}, {product: "Barry Manilow's Greatest Hits Colletion Vol 1", price: 500}, {product: "Chewbacca Mask", price: 3.41}, {product: "Woof Washer 360", price: 9.99}, {product: "Sauna Pants", price: 39.95}];
+var orders = [{product: "Space Cat T-shirt", price: 100}, 
+{product: "Barry Manilow's Greatest Hits Colletion Vol 1", price: 500}, 
+{product: "Chewbacca Mask", price: 3.41}, {product: "Woof Washer 360", price: 9.99}, 
+{product: "Sauna Pants", price: 39.95}];
 
-
+function finalBoss(p){
+  for (var x =0; x<orders.length; x++);
+var myOrders = document.createElement("p");
+}
   
-
+document.getElementById("finalBoss").innerHTML=myOrders;
+document.getElementById("finalBoss").addEventListener("click", myOrders);*/
 
 };
-
 
 
 
